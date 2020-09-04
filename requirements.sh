78963de71e6785bb074bebb657871f61d618f581
Ayush Jain <ayushjain2911@gmail.com>
2020-09-04 20:02:40 +0530

    update gitignore

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip3 install firebase-admin
+pip3 install ipython
+pip3 install nbformat
+pip3 install import-ipynb
+python -m pip install pynput
+pip3 install ipykernel
+ipython kernel install --user --name=all-spark
+pip3 install RPi.GPIO
